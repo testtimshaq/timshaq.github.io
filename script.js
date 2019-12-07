fe203ea6ae32f1839cefc71f4c1e0b9d8632e5f9
testtimshaq <58642417+testtimshaq@users.noreply.github.com>
2019-12-08 04:14:54 +0500

    init commit
    test

---
+++ script.js
@@ -0,0 +1,51 @@
+$(function(){
+
+	const disableScroll = function(disableScroll){
+		$('body').css({
+ 				 'height': '100%',
+ 				 'overflow': 'hidden'
+			})
+	};
+
+	const enableScroll = function(enableScroll){
+		$('body').css({
+			'height': '100%',
+ 			'overflow': 'auto'
+		});
+	};
+
+
+	// 	const disableScroll = function(){
+	// 	$('html','body').on('mousewheel',function(){
+	// 		return false;
+	// 	})
+	// };
+
+
+	// const enableScroll = function(){
+	// 	$('html','body').off('mousewheel');
+	// };
+
+	$('#changeButton')
+	.css({
+		'margin-left': '20px',
+		'margin-bottom': '40px'
+	})
+	.click(function(fadeIn){
+		$('#popup-container').fadeIn(400, disableScroll);
+		$('#popup').animate({
+			width: '200px',
+			height: '300px'
+		}, 400);
+	});
+
+	$('#popup-container').click(function(event){
+		if(event.target == this) {
+			$(this).fadeOut(400, enableScroll);
+			$('#popup').animate({
+			width: 0,
+			height: 0
+		}, 400);
+		}
+	})
+});
